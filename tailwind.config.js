80803cb9f080a7dd33f39e8bc3986a99e699a2ab
Y4GİZz <yagiznurduhan344@gmail.com>
2026-09-07 14:53:24 +0300

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6C5CE7',
+      },
+    },
+  },
+  plugins: [],
+}
